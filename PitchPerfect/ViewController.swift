d6c1dc1140be5056b4fc6082681aa73a8722a6fd
Hari Guntupalli <hari@Guests-MacBook-Pro.local>
2016-05-10 16:12:06 -0400

    initial code check in

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Hari Guntupalli on 5/7/16.
+//  Copyright © 2016 HarryTech. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
